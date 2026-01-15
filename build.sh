25503f4b9b382fce2fcef60fafba21598ea68de5
Mohammad Ashfaqul Hasan <2305102@ugrad.cse.buet.ac.bd>
2026-01-15 15:07:35 +0600

    Enable OCR support with Tesseract for scanned PDFs

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Install Tesseract OCR for scanned PDF support
+apt-get update && apt-get install -y tesseract-ocr tesseract-ocr-eng
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
